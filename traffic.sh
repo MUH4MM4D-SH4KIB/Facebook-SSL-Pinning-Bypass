1dda289e7d696efac0b257566930e5a776ced432
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-03-09 14:12:54 +0600

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -1,27 +1,58 @@
+POST /graphql HTTP/2
+
 host: b-graph.facebook.com
+
 x-fb-request-analytics-tags: {"network_tags":{"product":"350685531728","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
+
 x-fb-rmd: state=URL_ELIGIBLE
+
 priority: u=0
+
 content-encoding: gzip
+
+x-fb-device-group: 2027
+
+x-fb-integrity-machine-id: 0Xuuabs0NKqy9r67njaAPffo
+
 x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
-user-agent: [FBAN/FB4A;FBAV/549.0.0.61.62;FBBV/891619565;FBDM/{density=1.7125,width=720,height=1558};FBLC/en_US;FBRV/0;FBCR/TelKila;FBMF/Google;FBBD/Google;FBPN/com.facebook.katana;FBDV/Pixel;FBSV/10;FBOP/1;FBCA/arm64-v8a:;]
-x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.headers_process_transparency_event
-x-zero-f-device-id: 580cb910-b0cc-43fc-a14d-0ef9fd2b817a
+
+user-agent: [FBAN/FB4A;FBAV/551.1.0.58.63;FBBV/900039832;FBDM/{density=1.7125,width=720,height=1558};FBLC/en_US;FBRV/0;FBCR/TelKila;FBMF/Google;FBBD/Google;FBPN/com.facebook.katana;FBDV/Pixel;FBSV/10;FBOP/1;FBCA/arm64-v8a:;]
+
 x-graphql-request-purpose: fetch
-x-fb-device-group: 7650
+
+x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.send_login_request
+
+x-zero-f-device-id: ef45a8f2-8c58-44b4-aefd-23c8aa697aaf
+
 x-tigon-is-retry: False
-x-graphql-client-library: graphservice
-content-type: application/x-www-form-urlencoded
-x-fb-net-hni: 470007
-x-fb-sim-hni: 470007
-authorization: OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32
+
 x-zero-state: unknown
+
+x-graphql-client-library: graphservice
+
+x-fb-sim-hni: 470002
+
+content-type: application/x-www-form-urlencoded
+
+x-fb-net-hni: 470002
+
+authorization: OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32
+
 x-meta-zca: empty_token
-app-scope-id-header: 42ff3169-670a-4361-8a37-21ca999368cc
+
+app-scope-id-header: ef45a8f2-8c58-44b4-aefd-23c8aa697aaf
+
 x-fb-connection-type: WIFI
-x-meta-usdid: cce697a0-72ce-4459-9ca5-a570b5aa4286.1772440987.MEUCIDXgOg2gFXrJFIkpsgIr6axFz5freBNvkGiMHNOPDN7cAiEAkhMjJbxwV4li0uTH1eWZtv5DHXiBqpWynrcArfODjT0
+
+x-meta-usdid: d22eeaf0-d7d3-42b5-b48f-1c2e09f74792.1773047182.MEUCIErtWR3xcYTAkBuHGunHIgxsw32MXAT2_oLJ_Dk4vVlwAiEA9vKc0KoOyzU4RMSIzy0xYHsHdykgnCu3kkLVcTqDpk4
+
 accept-encoding: gzip, deflate
+
 x-fb-http-engine: Tigon/Liger
+
 x-fb-client-ip: True
+
 x-fb-server-cluster: True
-x-fb-conn-uuid-client: a2062371762a41bc2857fbd12869065b
+
+x-fb-conn-uuid-client: bb3f910377f0996184794c8497cb7a0a
+
