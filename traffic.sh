54f78ccb6caa5d8d129875c3218c9743e7222fb0
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-03-16 00:12:46 +0600

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -4,47 +4,41 @@ host: b-graph.facebook.com
 
 x-fb-request-analytics-tags: {"network_tags":{"product":"350685531728","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
 
-x-fb-rmd: state=URL_ELIGIBLE
-
 priority: u=0
 
 content-encoding: gzip
 
-x-fb-device-group: 2027
-
-x-fb-integrity-machine-id: 0Xuuabs0NKqy9r67njaAPffo
-
 x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
 
-user-agent: [FBAN/FB4A;FBAV/551.1.0.58.63;FBBV/900039832;FBDM/{density=1.7125,width=720,height=1558};FBLC/en_US;FBRV/0;FBCR/TelKila;FBMF/Google;FBBD/Google;FBPN/com.facebook.katana;FBDV/Pixel;FBSV/10;FBOP/1;FBCA/arm64-v8a:;]
+user-agent: [FBAN/FB4A;FBAV/552.1.0.45.68;FBBV/906186281;FBDM/{density=1.7125,width=720,height=1558};FBLC/en_US;FBRV/0;FBCR/TelKila;FBMF/Google;FBBD/Google;FBPN/com.facebook.katana;FBDV/Pixel;FBSV/10;FBOP/1;FBCA/arm64-v8a:;]
+
+x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.bloks.caa.request.attestation.key_gen
+
+x-zero-f-device-id: 04b8127f-d723-4e40-9bd3-ca37690bca0b
 
 x-graphql-request-purpose: fetch
 
-x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.send_login_request
-
-x-zero-f-device-id: ef45a8f2-8c58-44b4-aefd-23c8aa697aaf
+x-fb-device-group: 377
 
 x-tigon-is-retry: False
 
-x-zero-state: unknown
-
 x-graphql-client-library: graphservice
-
-x-fb-sim-hni: 470002
 
 content-type: application/x-www-form-urlencoded
 
 x-fb-net-hni: 470002
 
+x-fb-sim-hni: 470002
+
 authorization: OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32
+
+x-zero-state: unknown
 
 x-meta-zca: empty_token
 
-app-scope-id-header: ef45a8f2-8c58-44b4-aefd-23c8aa697aaf
+app-scope-id-header: 1cb598f7-41e7-4d87-90ae-832f11321fca
 
 x-fb-connection-type: WIFI
-
-x-meta-usdid: d22eeaf0-d7d3-42b5-b48f-1c2e09f74792.1773047182.MEUCIErtWR3xcYTAkBuHGunHIgxsw32MXAT2_oLJ_Dk4vVlwAiEA9vKc0KoOyzU4RMSIzy0xYHsHdykgnCu3kkLVcTqDpk4
 
 accept-encoding: gzip, deflate
 
@@ -54,5 +48,4 @@ x-fb-client-ip: True
 
 x-fb-server-cluster: True
 
-x-fb-conn-uuid-client: bb3f910377f0996184794c8497cb7a0a
-
+x-fb-conn-uuid-client: kTDsrCTfd44Z/t/MeR7Qnw==
